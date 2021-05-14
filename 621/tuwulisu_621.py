6467c8df5e5bda57187da46548708ade9adbe8ea
tuwulisu6110 <tuwulisu6110@gmail.com>
2021-05-14 22:11:08 +0800

    Update tuwulisu_621.py
    慢

--- 621/tuwulisu_621.py
+++ 621/tuwulisu_621.py
@@ -1,26 +1,36 @@
 class Solution:
     def leastInterval(self, tasks, n):
+        def maintain_priority(list_: list,index: int)->None:
+            while index != len(list_)-1 and list_[index]<list_[index+1]:
+                list_[index], list_[index+1] = list_[index+1], list_[index]
+                index += 1
         dict_ = dict()
-        set_ = set()
         for task in tasks:
-            set_.add(task)
             if task not in dict_:
                 dict_[task]=[1,float("-inf")]
             else:
                 dict_[task][0]+=1
+        priority_list=[]
+        for key,val in dict_.items():
+            priority_list.append([val[0],key])
+        priority_list.sort(key=lambda e:e[0],reverse=True)
         time=0
-        while len(set_)>0:
+        while len(priority_list)>0:
             picked_task=None
-            for task in iter(set_):
-                if dict_[task][1]+n < time:
+            for index,ele in enumerate(priority_list):
+                task=ele[1]
+                if task in dict_ and dict_[task][1]+n < time:
                     picked_task=task
+                    priority_list[index][0]-=1
+                    maintain_priority(priority_list,index)
+                    if priority_list[-1][0]==0:
+                        priority_list.pop()
                     break
             if picked_task:
                 dict_[picked_task][0]-=1
                 dict_[picked_task][1]=time
                 if dict_[picked_task][0] == 0:
-                    set_.remove(picked_task)
+                    dict_.pop(picked_task)
+            #print(picked_task)
             time+=1
         return time
-s = Solution()
-print(s.leastInterval(["A","A","A","B","B","B"],2))
