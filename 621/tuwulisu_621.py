33fccacd0ea574967368005b27f0f4f3cb31d043
Boris Chang <tuwulisu6110@gmail.com>
2021-05-14 18:36:13 +0800

    updated 621

--- 621/tuwulisu_621.py
+++ 621/tuwulisu_621.py
@@ -5,14 +5,14 @@ class Solution:
         for task in tasks:
             set_.add(task)
             if task not in dict_:
-                dict_[task]=[1,-1]
+                dict_[task]=[1,float("-inf")]
             else:
                 dict_[task][0]+=1
         time=0
         while len(set_)>0:
             picked_task=None
             for task in iter(set_):
-                if dict_[task][1] < time:
+                if dict_[task][1]+n < time:
                     picked_task=task
                     break
             if picked_task:
