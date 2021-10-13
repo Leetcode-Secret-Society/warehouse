e718f0c6c727045bbf9a70cbb4dd9607f1ed992d
TheFcat <ken199091@gmail.com>
2021-10-13 21:54:20 +0800

    implement 464

--- 464/Fcat_464_dp.py
+++ 464/Fcat_464_dp.py
@@ -12,13 +12,13 @@ class Solution:
         if key in self.win_map:
             return self.win_map[key]
 
-        if max(candidates) >= desiredTotal:
+        if candidates[-1] >= desiredTotal:
             return win_flag
 
         for i in range(len(candidates)):
             next_candidates = candidates[:i] + candidates[i+1:]
             result = self.check_win(next_candidates, desiredTotal-candidates[i], not win_flag)
-            if result is win_flag:
+            if result == win_flag:
                 self.win_map[key] = win_flag
                 return win_flag
         self.win_map[key] = not win_flag
