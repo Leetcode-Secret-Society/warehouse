046290024f19e233b9714e34e6a6407975264a81
TheFcat <ken199091@gmail.com>
2022-04-28 21:41:44 +0800

    implement 72

---
+++ 72/fcat_72.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        dp = [[0] * (len(word2)+1) for _ in range((len(word1))+1)]
+        for i in range(len(dp[0])):
+            dp[0][i] = i
+        for i in range(len(dp)):
+            dp[i][0] = i
+
+        for i in range(1, len(dp)):
+            for j in range(1, len(dp[0])):
+                if word1[i-1] != word2[j-1]:
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                else:
+                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1] - 1)
+        return dp[-1][-1]
