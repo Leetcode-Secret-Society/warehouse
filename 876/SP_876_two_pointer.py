766fc0cfeaaa212b043f0e39d71020f014f16e48
Splenden <Matsurika@hotmail.com.tw>
2022-05-06 16:41:22 +0800

    Create SP_876_two_pointer.py

---
+++ 876/SP_876_two_pointer.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        left = head
+        right = head
+        while right and right.next :
+            left = left.next
+            right = right.next.next
+        return left
