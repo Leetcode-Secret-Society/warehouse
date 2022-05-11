988939dff5a48daeae1262d9b1aa9a59041d9418
Splenden <Matsurika@hotmail.com.tw>
2022-05-11 17:50:36 +0800

    Create SP_617_bfs.py

---
+++ 617/SP_617_bfs.py
@@ -0,0 +1,23 @@
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+        
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        node = root
+        while node and node.left:#search horizontal level
+            print("*"+str(node.val))
+            next = node.left
+            while node: #search parrell level
+                print(node.val)
+                node.left.next = node.right
+                node.right.next = node.next and node.next.left #return node.next.left if node.next exists
+                node = node.next
+            node = next
+        return root
