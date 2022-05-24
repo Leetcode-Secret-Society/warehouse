69d3e5e330732ed14a2791b3aed7fcea42d4c7cb
Splenden <Matsurika@hotmail.com.tw>
2022-05-25 02:47:07 +0800

    Update SP_277_shorten.swift

--- 277/SP_277_shorten.swift
+++ 277/SP_277_shorten.swift
@@ -9,6 +9,7 @@ class Solution : Relation {
         var potentialCelebrity = 0
         var cache = [String: Bool]()
         func cacheKnows(_ a:Int, _ b:Int) -> Bool {
+            //swift tuple isn't hashable
             let key = "\(a),\(b)"
             if cache[key] == nil {
                 cache[key] = knows(a,b)
@@ -20,6 +21,7 @@ class Solution : Relation {
                 if people == target {
                     //not asking self
                 } else {
+                    //shorten logic..
                     if !cacheKnows(target, people) || cacheKnows(people, target) {
                         return false
                     }
@@ -29,6 +31,7 @@ class Solution : Relation {
         }
         for people in 0..<n {
             if cacheKnows(potentialCelebrity, people) {
+                //trying get most possible celebrity, which means at least one man knowing him, him might not know others
                 potentialCelebrity = people
             }
         }
