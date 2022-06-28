6afaa4b3102da889f5bf8cd03e55c5c4c97e1a41
TheFcat <ken199091@gmail.com>
2022-06-28 16:48:52 +0800

    implement 1115

---
+++ 1115/Fcat_1115.py
@@ -0,0 +1,21 @@
+import threading
+
+
+class FooBar:
+    def __init__(self, n):
+        self.n = n
+        self.foo_lock = threading.Lock()
+        self.bar_lock = threading.Lock()
+        self.bar_lock.acquire()
+
+    def foo(self, printFoo: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.foo_lock.acquire()
+            printFoo()
+            self.bar_lock.release()
+
+    def bar(self, printBar: 'Callable[[], None]') -> None:
+        for i in range(self.n):
+            self.bar_lock.acquire()
+            printBar()
+            self.foo_lock.release()
