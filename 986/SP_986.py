89165142c2d8c7123cc6e6f9ca02909ecd407b02
Splenden <Matsurika@hotmail.com.tw>
2022-07-04 16:39:36 +0800

    Create SP_986.py

---
+++ 986/SP_986.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        i = j = 0
+        result = []
+        while i < len(firstList) and j < len(secondList):
+            low = max(firstList[i][0], secondList[j][0])
+            high = min(firstList[i][1], secondList[j][1])
+            if low <= high:
+                result.append([low, high])
+            
+            #compare who's the smaller/eariler one
+            if firstList[i][1] < secondList[j][1]:
+                i += 1
+            else :
+                j += 1
+        return result
