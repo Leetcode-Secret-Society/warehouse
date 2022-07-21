a51ca3b283b60ac2fbeb059350cf0ab192d4edd8
Splenden <Matsurika@hotmail.com.tw>
2022-07-21 14:45:40 +0800

    Create SP_22_back_tracking.py

---
+++ 22/SP_22_back_tracking.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # 1. ()
+        # 2. ()() / (())
+        # 3. ()()()/ (())() ()(())/ ((()))
+        ans = []
+        limit = 2 * n
+        def backtrack(S = [], left = 0, right = 0):
+            #((( -> (((,)))
+            #(( -> ((,).()) -> ((,).)()
+            #( -> (,).(()) -> (,).().()
+            if left+right == limit:
+                ans.append("".join(S))
+                return
+            if left < n:
+                S.append("(")
+                backtrack(S, left+1, right)
+                S.pop()
+            if right < left:
+                S.append(")")
+                backtrack(S, left, right+1)
+                S.pop()
+        backtrack()
+        return ans
