f75865628e668f4ded7f3c555a8b46d7dcae3952
yahan.l <yahan.l@migo.io>
2023-01-11 14:58:18 +0800

    implement 377

--- 377/Fcat_377_dp.py
+++ 377/Fcat_377_dp.py
@@ -6,6 +6,8 @@ class Solution:
         def recursive(num):
             if num < 0:
                 return 0
+            if num == 0:
+                return 1
             if num in dp:
                 return dp[num]
             total = 0
@@ -13,14 +15,9 @@ class Solution:
                 total += recursive(num - n)
             dp[num] = total
             return total
-        dp = {}
-        nums.sort()
-        dp[nums[0]] = 1
-        for num in nums[1:]:
-            dp[num] = recursive(num)
-            dp[num] += 1
 
+        dp = {}
         return recursive(target)
 
 
-print(Solution().combinationSum4([4,2,1], 32))
+print(Solution().combinationSum4([4, 2, 1], 32))
