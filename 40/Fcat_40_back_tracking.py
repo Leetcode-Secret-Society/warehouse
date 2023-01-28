1c9be5b59a91a4eb3da05163baf45b50ca876705
yahan.l <yahan.l@migo.io>
2023-01-28 14:35:15 +0800

    implement 40

--- 40/Fcat_40_back_tracking.py
+++ 40/Fcat_40_back_tracking.py
@@ -5,17 +5,17 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
 
-        result = set()
+        result = []
 
         def recursive(cur_result, cur_sum, rest, index):
             if cur_sum + rest < target:
                 return
             if cur_sum == target:
-                result.add(tuple(cur_result))
+                result.append(list(cur_result))
             if index >= len(candidates) or cur_sum > target:
                 return
             for i in range(index, len(candidates)):
-                if i != index and candidates[i] == candidates[i-1]:
+                if i != index and candidates[i] == candidates[i - 1]:
                     continue
                 cur_result.append(candidates[i])
                 recursive(cur_result, cur_sum + candidates[i], rest - candidates[i], i + 1)
@@ -23,12 +23,8 @@ class Solution:
 
         total = sum(candidates)
         for i in range(len(candidates)):
-            if i != 0 and candidates[i] == candidates[i-1]:
+            if i != 0 and candidates[i] == candidates[i - 1]:
                 continue
             recursive([candidates[i]], candidates[i], total - candidates[i], i + 1)
 
-        list_result = []
-
-        for comb in result:
-            list_result.append(list(comb))
-        return list_result
+        return result
