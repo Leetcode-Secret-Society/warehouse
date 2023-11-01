33b3963f0530a140d5e55b19208a56f625bbd911
Splenden <Matsurika@hotmail.com.tw>
2023-11-01 17:28:33 +0800

    Create SP_394_stack.py

---
+++ 394/SP_394_stack.py
@@ -0,0 +1,28 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        #3[a]2[bc]d
+        #3[a2[b3[c]]]xyz
+        stack = []
+        current_digit = ""
+        digits = []
+        contains_brace = False
+        for i in range(len(s)):
+            c = s[i]
+            if c == "[":
+                digits.append(int(current_digit))
+                current_digit = ""
+                stack.append(c)
+            elif c == "]":
+                #get brace content
+                content = ""
+                while len(stack) > 0 and stack[-1] != "[":
+                    content += stack.pop()
+                stack.pop() # pop "["
+                digit = digits.pop()
+                stack.extend(digit * content[::-1]) #using extend but not append...
+            elif c.isdigit():
+                current_digit += c
+            else:
+                stack.append(c)
+            
+        return "".join(stack)
