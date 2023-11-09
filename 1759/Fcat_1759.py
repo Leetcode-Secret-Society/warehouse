1e0987d6c5719de6feaa97f460bc032f4647ac5d
TheFcat <ken199091@gmail.com>
2023-11-09 13:36:37 +0800

    implement 1759

--- 1759/Fcat_1759.py
+++ 1759/Fcat_1759.py
@@ -1,13 +1,13 @@
 class Solution:
     def countHomogenous(self, s: str) -> int:
-        continous = 0
+        count = 0
         current_c = None
         result = 0
         for c in s:
             if c != current_c:
-                result += (continous + 1) * continous // 2
+                result += (count + 1) * count // 2
                 current_c = c
-                continous = 0
-            continous += 1
-        result += (continous + 1) * continous // 2
+                count = 0
+            count += 1
+        result += (count + 1) * count // 2
         return result % (10 ** 9 + 7)
