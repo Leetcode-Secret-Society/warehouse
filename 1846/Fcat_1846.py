595832c6773f00a1da68677cbf465323ca61437b
TheFcat <ken199091@gmail.com>
2023-11-15 22:12:47 +0800

    implement 1846

---
+++ 1846/Fcat_1846.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        arr.sort()
+        arr[0] = 1
+        for i in range(1, len(arr)):
+            if arr[i] == arr[i - 1]:
+                continue
+            arr[i] = arr[i - 1] + 1
+        return arr[-1] - arr[0] + 1
