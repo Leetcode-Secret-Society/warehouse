d4c6a1c67dc028947e571311a36491a53718fe65
TheFcat <ken199091@gmail.com>
2023-11-24 09:38:55 +0800

    implement 1561

---
+++ 1561/Fcat_1561.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort()
+        result = 0
+        for i in range(len(piles) // 3, len(piles), 2):
+            result += piles[i]
+        return result
