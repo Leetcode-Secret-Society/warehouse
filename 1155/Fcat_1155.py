d3140405b7ec109b61305a869920adfb2376d042
TheFcat <ken199091@gmail.com>
2023-12-26 19:23:36 +0800

    implement 1155

--- 1155/Fcat_1155.py
+++ 1155/Fcat_1155.py
@@ -16,9 +16,9 @@ class Solution:
             else:
                 if n == 0:
                     return 0
-            numRolls = 0
+            num_rolls = 0
             for point in range(k, 0, -1):
-                numRolls += get_roll_ways(n - 1, k, target - point)
-            return numRolls % (10 ** 9 + 7)
+                num_rolls += get_roll_ways(n - 1, k, target - point)
+            return num_rolls % (10 ** 9 + 7)
 
         return get_roll_ways(n, k, target)
